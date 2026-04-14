befae93927a1754873ec4107f46a21f71381de58
bergad37 <bergad37@gmail.com>
2026-04-14 09:06:44 +0200

    fix: loading the model

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 from fastapi import FastAPI, UploadFile, File
 import os
-from scripts.predict import run_prediction
 import uuid
-
+from scripts.predict import run_prediction
 
 app = FastAPI()
+
+
+# ✅ ADD IT HERE (right after app = FastAPI())
+@app.on_event("startup")
+def startup_event():
+    print("🔥 Preloading models...")
+
+    from scripts.predict import load_models
+    load_models()
+
+    print("✅ Models ready")
+
 
 @app.get("/")
 def home():
     return {"status": "running"}
+
 
 @app.post("/predict")
 async def predict(file: UploadFile = File(...)):
     os.makedirs("temp", exist_ok=True)
 
     file_path = f"temp/{uuid.uuid4()}.jpg"
+
     with open(file_path, "wb") as f:
         f.write(await file.read())
 
